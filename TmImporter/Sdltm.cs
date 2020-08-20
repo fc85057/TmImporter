d9d2456d0b430e254456845c7f005ce13ed004f1
Ferst - EVS Translations <ferst@evs-translations.com>
2020-08-20 08:47:09 -0400

    Add project files.

---
+++ TmImporter/Sdltm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+
+namespace TmImporter
+{
+    class Sdltm
+    {
+        private FileBasedTranslationMemory tm;
+
+        public FileBasedTranslationMemory TmAccess
+        {
+            get { return tm; }
+        }
+
+        public string SourceLanguage
+        {
+            get
+            {
+                return tm.LanguageDirection.SourceLanguage.TwoLetterISOLanguageName;
+            }
+        }
+
+        public string TargetLanguage
+        {
+            get
+            {
+                return tm.LanguageDirection.TargetLanguage.TwoLetterISOLanguageName;
+            }
+        }
+
+        public string LanguageDirection
+        {
+            get
+            {
+                return (SourceLanguage + "-" + TargetLanguage).ToUpper();
+            }
+        }
+
+        public ITranslationMemoryLanguageDirection ExportSettings
+        {
+            get
+            {
+                return tm.LanguageDirection;
+            }
+        }
+
+        public int TranslationUnits
+        {
+            get
+            {
+                return tm.GetTranslationUnitCount();
+            }
+        }
+
+
+        public Sdltm(string path)
+        {
+            // need to load relevant properties from "internal" tm data
+            tm = new FileBasedTranslationMemory(path);
+        }
+
+    }
+}
